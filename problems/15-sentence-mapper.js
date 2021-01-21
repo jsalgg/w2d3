bfa5a1430da64ca53283975ae18cd245ad664bf2
Luke  Kingsley <mzootfb@Lukes-MBP.attlocal.net>
2021-01-21 18:36:47 -0500

    15 problem

--- problems/15-sentence-mapper.js
+++ problems/15-sentence-mapper.js
@@ -25,14 +25,33 @@ let result2 = sentenceMapper("this is pretty cool right", removeVowels);
 console.log(result2); // 'ths s prtty cl rght'
 *******************************************************************************/
 
-let sentenceMapper = function() {
+let sentenceMapper = function (str, cb) {
+  let wordsArr = str.split(" ");
+  let finalArr = wordsArr.map(function (ele) {
+    return cb(ele);
+  });
 
+  return finalArr.join(" ");
 };
 
+let result1 = sentenceMapper("what is the answer?", function (word) {
+  return word.toUpperCase() + "!";
+});
+console.log(result1); // 'WHAT! IS! THE! ANSWER?!'
 
+let removeVowels = function (word) {
+  let newWord = "";
+  for (let i = 0; i < word.length; i++) {
+    let char = word[i];
+    if (!"aeiou".includes(char)) {
+      newWord += char;
+    }
+  }
+  return newWord;
+};
 
-
-
+let result2 = sentenceMapper("this is pretty cool right", removeVowels);
+console.log(result2); // 'ths s prtty cl rght'
 
 /*****************DO NOT MODIFY ANYTHING UNDER THIS  LINE**********************/
 module.exports = sentenceMapper;
